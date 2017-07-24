e98636c3f7353ed480ca4c672ffcd74076bb2e83
Emily Deans <emilyldeans@gmail.com>
2017-07-24 15:43:28 -0400

    Utitlized moment and chalk npms to log the first three lines of required code

--- main.js
+++ main.js
@@ -14,5 +14,9 @@ const chalk = require('chalk');
 console.log(chalk.blue('Hello world!'));
 
 let date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+let dayNumber = moment().format("DDD") + "th";
+let seconds = moment().format("s");
 
-console.log("It is " + date);
+console.log("It is " + chalk.blue(date) + " .");
+console.log("It is the " + chalk.magenta(dayNumber) + " day of the year.");
+console.log("It is " + chalk.green(seconds) + " seconds into the day.");
