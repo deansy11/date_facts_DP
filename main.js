1bfa25489dd775a219867d86ca5fea5e186c6db4
Emily Deans <emilyldeans@gmail.com>
2017-07-24 15:24:17 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const moment = require('moment');
+moment().format();
+
+// requirejs.config({
+//   packages: [{
+//     name: 'moment',
+//     location: '[bower_components | node_modules]/moment',
+//     main: 'moment'
+//   }]
+// })
+
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+
+console.log("It is")
